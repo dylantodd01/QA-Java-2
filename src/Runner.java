3a06fe99ab90cc38a085c1e30c191b080d2a9935
Dan <ticket.servd@gmail.com>
2024-10-28 14:00:15 +0000

    string builder, loops

--- src/Runner.java
+++ src/Runner.java
@@ -1,5 +1,7 @@
 import com.qa.Tortoise;
 
+import java.sql.SQLSyntaxErrorException;
+
 public class Runner {
     public static void main(String[] args) {
         Tortoise shell = new Tortoise("Russian Tortoise", 18, 78);
@@ -13,6 +15,51 @@ public class Runner {
         System.out.println(bill);
 
         Tortoise.makeNoise();
+
+
+        Tortoise tilly = new Tortoise("Cool", 12, 12);
+        Tortoise ted = new Tortoise("Uncool", 13, 13);
+        Tortoise toby = new Tortoise("Very cool", 130, 15);
+
+        Tortoise[] torts = {tilly, ted, toby};
+
+        for (int i = 0; i < torts.length; i++) {
+            System.out.println(torts[i]);
+        }
+
+        System.out.println("MIDDLE OF LOOPS");
+
+        for (Tortoise tort: torts) {
+            System.out.println(tort);
+        }
+
+        System.out.println("String time");
+
+        String firstPart = "this is a stringy string";
+        String secondPart = "|| this is a second stringy string";
+
+        StringBuilder sb = new StringBuilder();
+
+        String answer = new StringBuilder(firstPart).append(".").append(secondPart).toString();
+
+        String secondAns = firstPart + "." + secondPart;
+
+        System.out.println(answer);
+
+
+        System.out.println("INSERT DELETE TIME");
+
+        String deletedAns = new StringBuilder(answer).delete(4,12).toString();
+
+        System.out.println("NEW --> " +deletedAns);
+        System.out.println("ORIG --> " + answer);
+
+        StringBuilder sbAns = new StringBuilder(answer);
+
+        System.out.println("INLINE --> " + sbAns.delete(5,15));
+        System.out.println(sbAns);
+
+        
 
     }
 
