b57de3e3457c15ae68c67a98521fc7162aebf5ba
Dylan Todd <dylantodd00@gmail.com>
2024-10-28 10:56:40 +0000

    Created tortoise class

---
+++ src/Runner.java
@@ -0,0 +1,18 @@
+import com.qa.Tortoise;
+
+public class Runner {
+    public static void main(String[] args) {
+        Tortoise shell = new Tortoise("Russian Tortoise", 18, 78);
+        Tortoise tony = new Tortoise("Red-Footed Tortoise", 54);
+        Tortoise bill = new Tortoise();
+
+        System.out.println(shell);
+        System.out.println(tony);
+
+        bill.setAge(46);
+        System.out.println(bill);
+
+        Tortoise.makeNoise();
+
+    }
+}
--- src/com/qa/Tortoise.java
+++ src/com/qa/Tortoise.java
@@ -5,4 +5,57 @@ public class Tortoise {
     private String species;
     private int weight;
     private int age;
+
+    // All args constructor
+    public Tortoise(String species, int weight, int age) {
+        this.species = species;
+        this.weight = weight;
+        this.age = age;
+    }
+
+    // No args constructor
+    public Tortoise() {}
+
+    public Tortoise(String species, int age) {
+        this(species, 15, age);
+    }
+
+    public static void makeNoise() {
+        System.out.println("Squeak");
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Tortoise{" +
+                "species='" + species + '\'' +
+                ", weight=" + weight +
+                ", age=" + age +
+                '}';
+    }
 }
